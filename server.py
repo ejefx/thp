b2c86aabc8fb465cef1d6649ad7eafe711188572
Aghod <ejedennis07@gmail.com>
2025-08-05 23:49:23 +0100

    Initial commit for Flask site

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # Or return send_file if using static file
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
